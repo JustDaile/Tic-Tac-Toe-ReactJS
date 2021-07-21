3a084918184cd06edec71fa0a371383713136a62
JustDaile <daile.alimo1991@hotmail.com>
2021-07-21 01:34:01 +0100

    3 by 3 - default game settings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Tic-Tac-Toe</header>
-      <Grid size={4} />
+      <Grid size={3} />
     </div>
   );
 }
